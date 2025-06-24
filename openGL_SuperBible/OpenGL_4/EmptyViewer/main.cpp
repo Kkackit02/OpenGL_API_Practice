07231691ba2e28409ebd465eb99102f1d4628ce8
GeunNyeong Jeong <kkackit@gmail.com>
2025-06-24 16:39:48 +0900

    ~4-4

--- openGL_SuperBible/OpenGL_4/EmptyViewer/main.cpp
+++ openGL_SuperBible/OpenGL_4/EmptyViewer/main.cpp
@@ -1,18 +1,13 @@
-﻿#define CURRENT_LAB 8  // ⭐ 여기 숫자만 바꿔서 실행할 lab 선택
+﻿#define CURRENT_LAB 4  // ⭐ 여기 숫자만 바꿔서 실행할 lab 선택
 
 #if CURRENT_LAB == 1
-#include "3-1.cpp"
+#include "4-1.cpp"
 #elif CURRENT_LAB == 2
-#include "3-2.cpp"
+#include "4-2.cpp"
 #elif CURRENT_LAB == 3
-#include "3-3.cpp"
+#include "4-3.cpp"
 #elif CURRENT_LAB == 4
-#include "3-4.cpp"
+#include "4-4.cpp"
 #elif CURRENT_LAB == 5
-#include "3-5.cpp"
-
-#elif CURRENT_LAB == 6
-#include "3-6.cpp"
-#elif CURRENT_LAB == 8
-#include "3-8.cpp"
+#include "4-5.cpp"
 #endif
